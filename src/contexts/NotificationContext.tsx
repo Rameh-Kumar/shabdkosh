24b8978921f1826241df07bee6048468b334b1b8
Ramesh-kumar <goatrameshsahni@gmail.com>
2025-05-20 11:56:22 +0530

    Fix case sensitivity issue in import path

--- src/contexts/NotificationContext.tsx
+++ src/contexts/NotificationContext.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, ReactNode } from 'react';
-import Notification from '../components/UI/Notification';
+import Notification from '../components/ui/Notification';
 
 export type NotificationType = 'info' | 'success' | 'warning' | 'error';
 

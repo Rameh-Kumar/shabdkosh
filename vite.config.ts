7532dca5da5759b21e82fa94e5c0fe54351bb71c
Ramesh-kumar <goatrameshsahni@gmail.com>
2025-08-01 07:49:41 +0530

    Update Vite configuration and service worker

--- vite.config.ts
+++ vite.config.ts
@@ -49,57 +49,9 @@ export default defineConfig({
         scope: '/',
         icons: [
           {
-            src: '/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'any'
-          },
-          {
-            src: '/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any'
-          },
-          {
-            src: '/icon-48x48.png',
-            sizes: '48x48',
-            type: 'image/png',
-            purpose: 'any'
-          },
-          {
-            src: '/icon-72x72.png',
-            sizes: '72x72',
-            type: 'image/png',
-            purpose: 'any'
-          },
-          {
-            src: '/icon-96x96.png',
-            sizes: '96x96',
-            type: 'image/png',
-            purpose: 'any'
-          },
-          {
-            src: '/icon-128x128.png',
-            sizes: '128x128',
-            type: 'image/png',
-            purpose: 'any'
-          },
-          {
-            src: '/icon-144x144.png',
-            sizes: '144x144',
-            type: 'image/png',
-            purpose: 'any'
-          },
-          {
-            src: '/icon-152x152.png',
-            sizes: '152x152',
-            type: 'image/png',
-            purpose: 'any'
-          },
-          {
-            src: '/icon-384x384.png',
-            sizes: '384x384',
-            type: 'image/png',
+            src: '/favicon.svg',
+            sizes: '48x48 72x72 96x96 128x128 144x144 152x152 192x192 384x384 512x512',
+            type: 'image/svg+xml',
             purpose: 'any'
           }
         ]
